d3a5b8e73115495bbe7de7631f382bcb51886cd5
UCDT <adrian.j.ulibarri@gmail.com>
2021-05-07 16:23:57 -0600

    initial icon test

--- assets/script.js
+++ assets/script.js
@@ -65,7 +65,7 @@ const showCity = (cityName) => {
 }
 
 const showWeather = function(weather) {
-  let weatherIcon = '';
+  
   //appends the data from the api to the id fields in the main display container
   $('#date span').html(moment().calendar('L'));
   $('#temp span').html(`${weather.current.temp} ${IMPERIAL ? "˚F" : "˚C"}`);
@@ -73,18 +73,43 @@ const showWeather = function(weather) {
   $('#uv span').html(weather.current.uvi);
   $('#hum span').html(`${weather.current.humidity} % `);
   
+  //thunderstorm codes
+  if (`${weather.current.weather[0].id}` == '200' || '201' || '202' || '210' || '211' || '212' || '221' || '230' || '231') {
+    $(`#weather-icon`).html(`<i class="fas fa-poo-storm fa-10x m-5"></i>`);
+    console.log(`${weather.current.weather[0].id}`)
+  } 
+  //drizzle codes
+  else if (`${weather.current.weather[0].id}` == '300' || '301' || '302' || '310' || '311' || '312' || '313' || '314' || '321') {
+    $(`#weather-icon`).html(`<i class="fas fa-cloud-rain fa-10x m-5"></i>`);
+    console.log(`${weather.current.weather[0].id}`)
+  }
+  //rain codes
+  else if (`${weather.current.weather[0].id}` == '500' || '501' || '502' || '503' || '504' || '520' || '521' || '522' || '531') {
+    $(`#weather-icon`).html(`<i class="fas fa-cloud-showers-heavy fa-10x m-5"></i>`);
+  }
+  //snow codes
+  else if (`${weather.current.weather[0].id}` == '600' || '601' || '602' || '611' || '612' || '613' || '615' || '616' || '620' || '621' || '622') {
+    $(`#weather-icon`).html(`<i class="fas fa-snowflake fa-10x m-5"></i>`);
+  }
+  //atmosphere codes
+  else if (`${weather.current.weather[0].id}` == '701' || '711' || '721' || '731' || '741' || '751' || '761' || '762' || '771' || '781') {
+    $(`#weather-icon`).html(`<i class="fas fa-smog fa-10x m-5"></i>`);
+  }
+  //cloud codes
+  else if (`${weather.current.weather[0].id}` == '801' || '802' || '803' || '804') {
+    $(`#weather-icon`).html(`<i class="fas fa-cloud ga-10x m-5"></i>`);
+  }
+  //clear codes
+  else {
+    $(`#weather-icon`).html(`<i class="fas fa-sun fa-10x m-5"></i>`);
+  }
+  
   // appends the data from the forecast api to the data fields in the 5 day containers
   for (let i = 0; i < 5; i++) {
     $(`#fivedate${[i]}`).html(moment().add(1, 'days').calendar('L'));
     $(`#fivetemp${[i]} span`).html(`${weather.daily[i].temp.day} ${IMPERIAL ? "˚F" : "˚C"}`)
     $(`#fivewind${[i]} span`).html(`${weather.daily[i].wind_speed} ${IMPERIAL ? "mph" : "m/s"}`)
     $(`#fivehum${[i]} span`).html(`${weather.daily[i].humidity}`)
-  }
-
-  if (`${weather.current.weather[0].main}` == `Clear`) {
-    $(`#weather-icon`).append(`<i class="fas fa-cloud-showers-heavy fa-10x m-5"></i>`);
-  } else {
-    $(`#weather-icon`).html(``)
   }
 }
 // make an for loop that cycles through all of the array objects and runs 2 other functions
