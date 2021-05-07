e3db811fa81c9098cd8638fe6ba1b08bc0baa4a4
UCDT <adrian.j.ulibarri@gmail.com>
2021-05-07 00:07:32 -0600

    finished favorites api functionality. added 5 day forecast html

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ const IMPERIAL = true;
 //callback to display the google api data
 const geoCoord = async function() {
   let searchTerm = $(`#search-input`).val();
-  console.log(searchTerm);
+  
   await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=1&appid=${oneCallApiKey}`)
   .then((response) => {
     response.json().then(res => {
@@ -34,45 +34,40 @@ const geoCoord = async function() {
   
 };
 
-
-
-//callback to display the one call api data
-
-
 //make search functionality that pulls an array of cities from the api
 
 const fetchCurrentWeather = async (city) => {
 
-  // let vari = cities.slice(0, 1);
-  // console.log(cities);
-  // console.log(vari);
-
-  // city = {
-  //   lat: "",
-  //   lon: ""
-  // }
   const lat = city.lat;
   const lon = city.lon;
 
-    let data = await fetch(`https://api.openweathermap.org/data/2.5/onecall?units=${IMPERIAL ? "imperial" : "metric"}&lat=${lat}&lon=${lon}&appid=${oneCallApiKey}`)
-      .then((response) => response.json())
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  let data = await fetch(`https://api.openweathermap.org/data/2.5/onecall?units=${IMPERIAL ? "imperial" : "metric"}&lat=${lat}&lon=${lon}&appid=${oneCallApiKey}`)
+    .then((response) => response.json())
+    .then(data => {
+      // console.log(data);
+      return data;
 
-    return data;
+    })
+    
+    .catch(error => {
+      console.error(error);
+    });
+
+  return data;
 
 
 };
 
 
+const showCity = (cityName) => {
+  console.log(cityName);
+  // $(`#city-append`).html(`${city.cityName}`);
+}
 
-const showWeather = weather => {
-
+const showWeather = function(weather) {
+  
   //appends the data from the api to the id fields in the main display container
+  
   $('#temp span').html(`${weather.current.temp} ${IMPERIAL ? "˚F" : "˚C"}`);
   $('#wind span').html(`${weather.current.wind_speed} ${IMPERIAL ? "mph" : "m/s"} `);
   $('#uv span').html(weather.current.uvi);
@@ -88,7 +83,7 @@ const showWeather = weather => {
 
 //save to local storage function
 // saveLocal = function() {
-//     localStorage.setItem("weather", JSON.stringify(cities));
+//     localStorage.setItem(city, JSON.stringify(cities));
 // };
 
 // load function
@@ -100,6 +95,7 @@ const showWeather = weather => {
 //     }
 
 // }
+
 // make an on click function that clears the display and forecast sections and re-applys the data and 5 day forecast
 // possible a second array need to look at api data
 
@@ -109,8 +105,6 @@ $(`#search`).click(async function(event) {
     
     geoCoord();
 
-    
-    
     // if (city in cities) {
     //   fetchWeather(cities[city])
     // } else {
@@ -118,13 +112,22 @@ $(`#search`).click(async function(event) {
     // }
 
     $(`.favorites`).append(`
-      <button class="col col-12 btn btn-info mt-2 mb-2 text-dark border-dark"><i class="fa fa-building-o" aria-hidden="true"></i>${search_input}</button>
+      <button class="col col-12 btn btn-info mt-2 mb-2 text-dark border-dark"><h4>${search_input}</h4></button>
     `).click((e) => {
-      console.log(e, 'hello');
+      
+      let cityName = (e.target.outerText);
+      let matchingCity = {}
+      // cycles through all of the object named city in the cities arry and looks for the name property
+      // when it finds one with the same name as the e.target.outerText it overwrites the block scoped
+      //matching cities object with that city object then runs 
+      for (city of cities) {
+        
+        if (city.cityName.toLowerCase() == cityName.toLowerCase()) {
+          matchingCity = city
+        }
+      } 
+      fetchCurrentWeather(matchingCity)
+      .then(weather => showWeather(weather))
     })
 });
 
-// blue buttons
-// search cities for city and run with city data
-// fetchCurrentweather();
-// fetchFiveDay();
